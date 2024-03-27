b1b53e61b4409765ec8ec16417e5f5c386fec2a1
Vladislav Doster <mvdoster@gmail.com>
2024-03-27 08:18:05 -0500

    refactor: rm git aliases & misc cleanup/additions
    Signed-off-by: Vladislav Doster <mvdoster@gmail.com>

--- zsh-aliases.plugin.zsh
+++ zsh-aliases.plugin.zsh
@@ -1,60 +1,52 @@
 #!/usr/bin/env zsh
+# vim: set expandtab filetype=zsh shiftwidth=4 softtabstop=4 tabstop=4:
 
 # According to the standard:
 # https://zdharma-continuum.github.io/Zsh-100-Commits-Club/Zsh-Plugin-Standard.html
-0="${ZERO:-${${0:#$ZSH_ARGZERO}:-${(%):-%N}}}"
-0="${${(M)0:#/*}:-$PWD/$0}"
 
-emulate -L zsh
+0=${ZERO:-${${0:#$ZSH_ARGZERO}:-${(%):-%N}}}
+0=${${(M)0:#/*}:-$PWD/$0}
 
-alias -- '+x'='chmod +x'
-alias -- '..'='builtin cd ../ && l'
-alias -- '...'='builtin cd ../../ && l'
-alias -- '....'='builtin cd ../../../ && l'
-alias -- '.....'='builtin cd ../../../../ && l'
-alias -- '?'='which'
-alias b='cd -'
-alias rmr='rm -rf'
+alias clist='mdfind -onlyin . -interpret .cpp & mdfind -onlyin . -interpret .cc'
+alias cjump='nvim $(mdfind -onlyin . -name main | grep -A 1 /Users)'
 
-alias -g B='brew'
-alias -g C='cat'
-alias -g D='docker'
-alias -g G='grep -R'
-alias -g L='less'
-alias -g P='builtin print'
+emulate -LR zsh
+setopt no_aliases
 
-alias get-path='print -l $path[@]'
-alias get-sys='print -l "OSTYPE=${OSTYPE}" "VENDOR=$VENDOR" "MACHTYPE=${MACHTYPE}" "CPUTYPE=${CPUTYPE}" "hardware=$(uname -m)" "processor=$(uname -p)"'
-alias get-user='print -P "%F{blue}$(whoami)%f @ %F{cyan}$(uname -a)%f"'
+alias -- '+x'='command chmod +x'
+alias -- '..'='builtin cd -- ../ && l'
+alias -- '...'='builtin cd -- ../../ && l'
+alias -- '....'='builtin cd -- ../../../ && l'
+alias -- '.....'='builtin cd -- ../../../../ && l'
+alias -- '?'='command which'
+alias b='builtin cd -'
+alias rmr='command rm -rf --'
 
-alias mk='make'
-alias mkcd='{ local DIR_NAME="$(cat -)"; command mkdir -p -- "$DIR_NAME" && builtin cd -P -- $DIR_NAME }<<<'
+alias -g -- B='command brew'
+alias -g -- C='command cat'
+alias -g -- D='command docker'
+alias -g -- G='command grep -R'
+alias -g -- L='command less'
+alias -g -- P='builtin print -P'
+alias -g -- g='command git'
+
+alias mk='command make'
+alias mkcd='{ local DIR_NAME=$(cat -) && command mkdir -p -- $DIR_NAME && builtin cd -P -- $DIR_NAME }<<<'
 
 alias scratchpad='${EDITOR:-vim} $(mktemp -t scratch.XXX.md)'
 alias rsync='rsync -azP'
 
-alias -g g='\git'
-alias ga='g add'
-alias gau='ga --update'
-alias gc='g commit --signoff'
-alias gca='gc --amend --reset-author'
-alias gcm='gc --message'
-alias gd='g diff'
-alias gds='gd --staged'
-alias gnuke='g clean -fxd && g reset --hard'
-alias gps='g pull'
-alias gps='g push'
-alias gr='g reset'
-alias grs='gr --staged'
-alias grv='g remote --verbose'
-alias gs='g status'
-alias git-repo-website='open $(g remote get-url origin)'
+alias -- git-repo-website='open $(git remote get-url origin)'
 
-alias -g l='command -p ls --color'
-alias la='l -a'
-alias ll='l -l'
+alias -- l='builtin ls --color'
+alias -- la='builtin ls -a'
+alias -- ll='builtin ls -l'
 
-alias cfg='builtin cd ${XDG_CONFIG_DIR:-$HOME/.config/} && l'
-alias ezc='${EDITOR:-vim} ${ZDOTDIR:-$HOME}/.zshrc'
-alias zdd='builtin cd ${ZDOTDIR:-$HOME/.config/zsh} && l'
-alias zrld='builtin exec zsh'
+alias -- cfg='builtin cd ${XDG_CONFIG_DIR:-$HOME/.config/} && l'
+alias -- ezc='${EDITOR:-vim} ${ZDOTDIR:-$HOME}/.zshrc'
+alias -- zdd='builtin cd ${ZDOTDIR:-$HOME/.config/zsh} && l'
+alias -- zrld='builtin exec zsh -l'
+
+alias -- get-path='print -l -- ${path[@]}'
+alias -- get-sys='print -l -- OSTYPE=${(qq)OSTYPE} VENDOR=${(qq)VENDOR} MACHTYPE=${(qq)MACHTYPE} CPUTYPE=${(qq)CPUTYPE} hardware=${(qq)$(uname -m)} processor=${(qq)$(uname -p)}'
+alias -- get-user='print -P -- %F{blue}$(whoami)%f @ %F{cyan}$(uname -a)%f'
